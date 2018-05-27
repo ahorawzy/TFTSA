f1fb1bbe77df2fcabaf27aa6802ef507accfe647
ZhiYuan Wang <lzyz09193@163.com>
2018-05-27 17:41:04 +0800

    Add a experiment(20180527) about flowknn function to experiment/ folder.

---
+++ experiment/flowknn_static_001.R
@@ -0,0 +1,34 @@
+# Experiment Title: Flow predicting using knn.
+# Date: 2018-05-27
+# Description: This experiment aims to check the usability of function "flowknn"
+# and use it in static predicting. The object traffic flow is 6th of Octuber,
+# the flow-base contains all flows except for the object traffic flow.
+
+# -----------------------------------------------------------------------
+
+# 1. Read the two wide flow datasets.
+# One is raw data for object flow and flow-base,
+# the other is loess data for flow-base.
+tmlzzloess <- read.csv("data-raw/tmlzzloess.csv")
+tmlzznew <- read.csv("data-raw/tmlzznew.csv",header = T)
+
+rownames(tmlzzloess) <- tmlzzloess[,1]
+tmlzzloess <- tmlzzloess[,-1]
+
+rownames(tmlzznew) <- tmlzznew[,1]
+tmlzznew <- tmlzznew[,-1]
+
+# -----------------------------------------------------------------------
+
+# 2. flow-base is raw data
+
+## 2.1 object flow and flow-base identify
+tmlobj <- tmlzznew[6,]
+tmlbase <- tmlzznew[-6,]
+
+## 2.2 execute flowknn function
+pre1006 <- flowknn(tmlobj,tmlbase,start = 73,k = 3,lag_duration = 24,fore_duration = 12)
+
+## 2.3 present outcome
+plot(1:288,pre1006,type="l",col="red")
+points(1:288,tmlobj,col="blue")
