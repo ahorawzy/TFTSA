e67e1c3dbddaf1a48d37fcad85da9fb3a3145ac1
ZhiYuan Wang <lzyz09193@163.com>
2018-05-27 16:44:55 +0800

    Use roxygen to generate document for handle_loess function.

--- R/handle_loess.R
+++ R/handle_loess.R
@@ -1,5 +1,24 @@
+#' Caculate LOESS value from a dataframe.
+#'
+#' This function caculates LOESS(locally weighted regression) by given a dataframe.
+#'
+#' LOESS (locally weighted regression) is a technique which can smooth a series. It
+#' needs a explaining variable as exp, a response variable as resp, a parameter span.
+#' By using loess() function in stats package, this function can caculate LOESS value
+#' for a series from a dataframe.
+#'
+#' @seealso \code{\link[stats]{loess}}
+#'
+#' @param df The dataFrame contains series to be loess.
+#' @param exp The sequence column number of explaining variable,
+#' which is a column of df.
+#' @param resp The sequence column number of response variable,
+#' which is another column of df.
+#' @param sp Span parameter of loess() function.
+#' @export
+
 handle_loess <- function(df,exp,resp,sp){
   result <- rep(NA,nrow(df))
-  result <- loess(df[,resp]~df[,exp],span=sp)
+  result <- stats::loess(df[,resp]~df[,exp],span=sp)
   return(result$fitted)
 }
