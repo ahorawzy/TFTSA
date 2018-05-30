552e671282f12e5afcad43a7d3b0e8aef96e2682
ZhiYuan Wang <lzyz09193@163.com>
2018-05-30 16:38:15 +0800

    Enhance flow_evaluate as #3.

--- R/flow_evaluate.R
+++ R/flow_evaluate.R
@@ -5,16 +5,22 @@
 #' @param rv real value of traffic flow, a row from a dataframe;
 #' @param pv forecast value, can be the return value of flow_knn function,
 #'           a row from a dataframe;
-#' @param method can be "mse", "rmse" or "mae"
+#' @param method can be "mse", "rmse", "mae" or "all"
 #' @export
-flow_evaluate <- function(rv,pv,method="mse"){
+flow_evaluate <- function(rv,pv,method="all"){
   n <- length(rv)
-  if(method=="mse"){
-    result <- 1/n*sum((rv-pv)**2)
-  } else if(method=="rmse"){
-    result <- sqrt(1/n*sum((rv-pv)**2))
-  } else if(method=="mae"){
-    result <- 1/n*sum(abs(rv-pv))
+  mse <- 1/n*sum((rv-pv)**2)
+  rmse <- sqrt(1/n*sum((rv-pv)**2))
+  mae <- 1/n*sum(abs(rv-pv))
+
+  if(method == "all"){
+    result <- data.frame(mse,rmse,mae)
+  } else if(method == "mse"){
+    result <- mse
+  } else if(method == "rmse"){
+    result <- rmse
+  } else if(method == "mae"){
+    result <- mae
   }
   return(result)
 }
