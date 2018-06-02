74e73699ae9ebeee1cd15e39868111a5b25bbd29
ahorawzy <lzyz09193@163.com>
2018-06-02 21:45:16 +0800

    Add document for dist_imbalance function.

--- R/dist_imbalance.R
+++ R/dist_imbalance.R
@@ -1,3 +1,19 @@
+#бо Imbalance distance caculation
+#'
+#' This functions help function knn_flow() use imbalance distance in caculating similarity
+#' among traffic flows.
+#'
+#' Imbalance distance means that values of flow in flow base is treated differently, which
+#' depends on whether it's higher than object flow or otherwise. Because the punishment of
+#' forecast should be different whether the forecast tends to be higher or smaller. It's more
+#' tolerable that make higher forecast than lower.
+#'
+#' @param obj The object flow (fragment) to be forecasted.
+#' @param base The flow base (fragment) used by KNN method.
+#' @return The distance between object flow and every flow in flow base as a dataframe with
+#' the same order of flow in flow base.
+#' @export
+
 dist_imbalance <- function(obj,base){
   n <- nrow(base)
   s <- matrix(NA,n,1)
